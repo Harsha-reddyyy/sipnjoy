9663e868eb935ce397b53be7f25936a96265e1a1
Harsha-reddyyy <harshavardhanreddy.0712@gmailcom>
2026-04-25 14:41:34 +0530

    changed tailwind

--- server/server.js
+++ server/server.js
@@ -17,10 +17,7 @@ app.use(cors());
 // Connect to database
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
     console.error(`Error: ${err.message}`);
